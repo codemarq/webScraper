536c99aacdd4c19cdf156131e07efd7a44d06162
John-Mike Marquardt <jmmarquardt@gmail.com>
2016-12-12 00:11:18 -0500

    routes

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,54 +1,124 @@
-// routes
-
-/ ==================================================================================
 // dependencies
-// ==================================================================================
-
-// require express
 var express = require('express');
-// initiate router
 var router = express.Router();
-
+var app = express();
 // require the model .js file
+// Requiring our Note and Article models
+var Note = require("../models/Note.js");
+var Article = require("../models/Article.js");
+var request = require('request');
+var cheerio = require('cheerio');
 
-
-// ==================================================================================
+// ============================================================================
 // ROUTES
-// ==================================================================================
+// ============================================================================
 
+// root
 router.get('/', function (req, res) {
-	res.render('/index');
+	res.render('index');
 });
 
-// route to handle select all
-router.get('/comments', function (req, res) {
-	comment.selectAll(function (data) {
+// A GET request to scrape the echojs website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  request("http://www.theonion.com/", function(error, response, html) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(html);
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
 
-		var hbsObject = {
-			comments: data
-		};
+      // Save an empty result object
+      var result = {};
 
-		// console.log(hbsObject);
-		res.render('index', hbsObject);
-	});
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this).children("a").text();
+      result.link = $(this).children("a").attr("href");
+
+      // Using our Article model, create a new entry
+      // This effectively passes the result object to the entry (and the title and link)
+      var entry = new Article(result);
+
+      // Now, save that entry to the db
+      entry.save(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        // Or log the doc
+        else {
+          console.log(doc);
+        }
+      });
+
+    });
+  });
+  // Tell the browser that we finished scraping the text
+  res.send("Scrape Complete");
 });
 
-// route to handle adding a new comment
-router.post('/comments/insert', function (req, res) {
-	comment.insertOne(['comment_name'], [req.body.comment_name], function () {
-		res.redirect('/comments');
-	});
+// This will get the articles we scraped from the mongoDB
+app.get("/articles", function(req, res) {
+  // Grab every doc in the Articles array
+  Article.find({}, function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Or send the doc to the browser as a json object
+    else {
+      res.json(doc);
+    }
+  });
 });
 
-// route to handle updating a comment (devouring it)
-router.put('/comments/update/:id', function (req, res) {
-	var condition = 'id = ' + req.params.id;
+// Grab an article by it's ObjectId
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  Article.findOne({ "_id": req.params.id })
+  // ..and populate all of the notes associated with it
+  .populate("note")
+  // now, execute our query
+  .exec(function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise, send the doc to the browser as a json object
+    else {
+      res.json(doc);
+    }
+  });
+});
 
-	// console.log('condition', condition);
 
-	comment.updateOne({devoured: true}, condition, function () {
-		res.redirect('/comments');
-	});
+// Create a new note or replace an existing note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  var newNote = new Note(req.body);
+
+  // And save the new note the db
+  newNote.save(function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise
+    else {
+      // Use the article id to find and update it's note
+      Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
+      // Execute the above query
+      .exec(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        else {
+          // Or send the document to the browser
+          res.send(doc);
+        }
+      });
+    }
+  });
 });
 
 // export router
--- server.js
+++ server.js
@@ -2,46 +2,58 @@
 // package dependencies
 // ==========================================================================
 var express = require('express');
+var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var request = require('request');
 var cheerio = require('cheerio');
-// models
 
 // mongoose mpromise is deprecated, using bluebird promises
 var Promise = require('bluebird');
 mongoose.Promise = Promise;
 
+// ==========================================================================
 // Sets up the Express App
 // ==========================================================================
 var app = express();
 var PORT = 3010;
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() || __dirname + '/public'));
-
+// Use morgan for logging and body-parser
+app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
 
-// override with POST having ?_method=DELETE
-app.use(methodOverride('_method'));
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() || __dirname + '/public'));
 
-var exphbs = require('express-handlebars');
-
+// handlebars for view engine using default layout "main"
 app.engine('handlebars', exphbs({
 	defaultLayout: 'main'
 }));
-
 app.set('view engine', 'handlebars');
 
+// ===========================================================================
+// MONGOOSE
+// ===========================================================================
+// config mongoDB with mongoose
+mongoose.connect("mongodb://localhost/onion");
+var db = mongoose.connection;
+
+// log mongoose errors
+db.on("error", function(error) {
+	console.log("Mongoose Error: ", error);
+});
+// log successful connection
+db.once("open", function(){
+	console.log("Mongoose connection successful.");
+});
+
+// ===========================================================================
+// Routes
+// ===========================================================================
 var routes = require('./controllers/controller.js');
-
-var models = require('./models');
-
-// models.sequelize.sync({force:true})
-
 app.use('/', routes);
 
 // Starts the server to begin listening
